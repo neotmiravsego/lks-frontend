3e7819fa54db50e26c1b5f63a0b77d1d61e7949f
neotmiravsego@gmail.com <neotmiravsego@gmail.com>
2019-09-09 11:59:42 +0200

    add components files

---
+++ components/header.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  .header__component
+    h1.title привет
+</template>
+
+<style lang="scss">
+.title {
+  color: red;
+}
+</style>
---
+++ pages/blog.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .container
+    vHeader  
+</template>
+
+<script>
+import vHeader from '~/components/header.vue'
+
+export default {
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  .title {
+    height: 200px;
+    color: $light;
+  }
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
