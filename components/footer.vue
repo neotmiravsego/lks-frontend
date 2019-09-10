6ca94df222715891fbf45caf820b9f79a182b990
neotmiravsego@gmail.com <neotmiravsego@gmail.com>
2019-09-10 23:15:14 +0200

    add components files

---
+++ components/footer.vue
@@ -0,0 +1,161 @@
+<template lang="pug">
+    footer
+      .footer__component__top__bar
+        .footer__panel
+          .footer__panel__navigation__wrap
+            h3.footer__panel__navigation__title Навигация
+            ul.footer__panel__navigation__list
+              li.footer__panel__navigation__item
+                a(href="#").footer__panel__navigation__link Блог
+              li.footer__panel__navigation__item
+                a(href="#").footer__panel__navigation__link Магазин
+              li.footer__panel__navigation__item
+                a(href="#").footer__panel__navigation__link О нас
+              li.footer__panel__navigation__item
+                a(href="#").footer__panel__navigation__link Портфолио
+              li.footer__panel__navigation__item
+                a(href="#").footer__panel__navigation__link Контакты
+          .footer__panel__controls__wrap
+            h3.footer__panel__controls__title Личный кабинет
+            ul.footer__panel__controls__list
+              li.footer__panel__controls__item
+                a(href="#").footer__panel__controls__link Войти
+              li.footer__panel__controls__item
+                a(href="#").footer__panel__controls__link Корзина
+              li.footer__panel__controls__item
+                a(href="#").footer__panel__controls__link Сохраненные товары
+        form.footer__form
+          input.footer__input(
+            type='sumbit'
+            placeholder='Ваш E-mail'
+            )
+          button.footer__button Подписаться
+          .footer__form__txt__wrap
+            p.footer__form__txt
+              | Мы проводим специальные акции для наших клиентов.
+              | оформите подписку и мы будем держать вас в курсе
+        .footer__social__list__wrap
+          ul.footer__social__list
+            li.footer__social__list__item
+              a(href="#").footer__social__item__link
+                img(src="../assets/images/svg/fbFooter.svg", alt="alt")
+            li.footer__social__list__item
+              a(href="#").footer__social__item__link
+                img(src="../assets/images/svg/instagramFooter.svg", alt="alt")
+            li.footer__social__list__item
+              a(href="#").footer__social__item__link
+                img(src="../assets/images/svg/vkFooter.svg", alt="alt")
+            li.footer__social__list__item
+              a(href="#").footer__social__item__link
+                img(src="../assets/images/svg/prsFooter.svg", alt="alt")
+      .footer__component__bottom__bar__wrap
+        .footer__component__bottom__bar
+          .copyright__wrap Little Knits Story 2017 | All Rights Reserved
+          .footer__logo
+            img(src="../assets/images/logo_footer.png", alt="alt").footer__logo__img
+          .privacy__policy
+            a(href="#").privacy__policy__link Политика конфеденциальности
+
+</template>
+
+<style lang="scss">
+footer {
+  border: 1px solid #e5e5e5;
+}
+.footer__component__top__bar {
+  padding: 24px 0;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 1100px;
+}
+.footer__panel {
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: space-between;
+}
+.footer__form {
+  margin: 0 auto;
+  position: relative;
+  width: 100%;
+  max-width: 950px;
+}
+.footer__input {
+  margin-bottom: 16px;
+  width: 100%;
+  height: 30px;
+  max-width: 950px;
+  border-radius: 20px;
+  outline: none;
+  border: none;
+  background: #f0f0f0;
+}
+.footer__panel__navigation__title {
+  margin-bottom: 18px;
+}
+.footer__panel__navigation__list {
+  display: flex;
+}
+.footer__panel__navigation__item {
+  padding-right: 40px;
+}
+.footer__panel__navigation__item:last-child {
+  padding-right: 0;
+}
+.footer__panel__controls__list {
+  display: flex;
+}
+.footer__panel__controls__title {
+  margin-bottom: 16px;
+}
+.footer__panel__controls__item {
+  padding-right: 40px;
+}
+.footer__panel__controls__item:last-child {
+  padding-right: 0px;
+}
+.footer__input {
+  padding: 0 10px;
+}
+.footer__button {
+  padding: 6px 50px;
+  position: absolute;
+  right: 0;
+  border-radius: 20px;
+  outline: none;
+  border: none;
+  background: #e0c0c3;
+  color: #fff;
+  font-size: $fontSize2;
+}
+.footer__form__txt__wrap {
+  margin: 0 auto;
+  margin-bottom: 24px;
+  width: 100%;
+  max-width: 622px;
+  text-align: center;
+  color: $neutral;
+}
+.footer__social__list {
+  display: flex;
+  justify-content: center;
+}
+.footer__social__list__item {
+  padding-right: 32px;
+}
+.footer__social__list__item:last-child {
+  padding-right: 0;
+}
+.footer__component__bottom__bar {
+  margin: 0 auto;
+  padding: 20px 400px 32px;
+  display: flex;
+  flex-wrap: wrap;
+  border: 1px solid #e5e5e5;
+  align-items: center;
+  justify-content: space-between;
+  color: $neutral;
+}
+.privacy__policy__link {
+  color: $neutral;
+}
+</style>
--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,123 @@
 <template lang="pug">
-  .header__component
-    h1.title привет
+  header
+    .header__component
+      .header__title__wrap
+        p.header__label Блог и магазин по вязанию
+        h1.header__title Little Kniths Story
+      .header__bottom__bar
+        .header__socials
+          ul.header__socials__list
+            li.header__social__list__item
+              a(href="#").header__social__link.facebook
+                img.headerSocial__img(src="../assets/images/svg/vk.svg")
+            li.header__social__list__item
+              a(href="#").header__social__link.facebook
+                img.headerSocial__img(src="../assets/images/svg/facebook.svg")
+            li.header__social__list__item
+              a(href="#").header__social__link.facebook
+                img.headerSocial__img(src="../assets/images/svg/instagram.svg")
+            li.header__social__list__item
+              a(href="#").header__social__link.facebook
+                img.headerSocial__img(src="../assets/images/svg/lid.svg")
+        nav.navigation
+          ul.navigation__list
+            li.navigation__list__item
+              a(href="#").navigation__list__item__link блог
+            li.navigation__list__item
+              a(href="#").navigation__list__item__link магазин
+            li.navigation__list__item
+              a(href="#").navigation__list__item__link авторы
+            li.navigation__list__item
+              a(href="#").navigation__list__item__link контакты
+            li.navigation__list__item
+              a(href="#").navigation__list__item__link схемы
+        .header__controls
+          ul.header__controls__list
+            li.header__controls__list__item
+              a(href="#").header__controls__link
+                img.header__controls__img(src="../assets/images/svg/login.svg")
+            li.header__controls__list__item
+              a(href="#").header__controls__link
+                img.header__controls__img(src="../assets/images/svg/shopping-bag.svg")
+            li.header__controls__list__item
+              a(href="#").header__controls__link
+                img.header__controls__img(src="../assets/images/svg/heart-shape-outline.svg")
+    .header__button__lang
+      button.header__button
 </template>
 
-<style lang="scss">
-.title {
-  color: red;
+<style lang="scss" scoped>
+.header {
+  position: relative;
+}
+.header__component {
+  margin: 0 auto;
+  margin-bottom: 32px;
+  padding: 40px 0 0;
+  width: 100%;
+  max-width: 1111px;
+  text-align: center;
+  font-size: $fontSize1;
+}
+.header__label {
+  margin-bottom: 16px;
+  font-family: $ffFranc;
+}
+.header__title {
+  font-size: 36px;
+  text-transform: uppercase;
+  color: #8a8e99;
+}
+.header__title__wrap {
+  margin-bottom: 45px;
+}
+.header__socials__list {
+  display: flex;
+  align-items: center;
+}
+.header__social__list__item {
+  margin-right: 28px;
+}
+.header__social__list__item:last-child {
+  margin-right: 0;
+}
+.navigation__list {
+  display: flex;
+}
+.navigation__list__item {
+  margin-right: 40px;
+  text-transform: uppercase;
+  font-family: $ffFranc;
+}
+.navigation__list__item:last-child {
+  margin-right: 0;
+}
+.header__controls__list {
+  display: flex;
+  align-items: center;
+}
+.header__controls__list__item {
+  margin-right: 50px;
+}
+.header__controls__list__item:last-child {
+  margin-right: 0;
+}
+.header__bottom__bar {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+}
+.header__button__lang {
+  position: absolute;
+  right: 20px;
+  top: 0;
+  width: 50px;
+  height: 50px;
+}
+.header__button {
+  width: 100%;
+  height: 100%;
+  background: orange;
 }
 </style>
