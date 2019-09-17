3d84001ce330fb3b7e2eb73ae97c7309f71bf78f
neotmiravsego@gmail.com <neotmiravsego@gmail.com>
2019-09-17 13:48:06 +0200

    add new component

---
+++ components/slider.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .sliderC
+</template>
+
+<style lang="scss">
+.sliderC {
+  margin: 0 auto;
+  margin-bottom: 40px;
+  width: 100%;
+  height: 352px;
+  max-width: $widthContainer1;
+  background: black;
+}
+</style>
