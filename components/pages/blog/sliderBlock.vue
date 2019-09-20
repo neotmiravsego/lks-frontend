44bbf156f3a4f2ed6513c255ccf7d4d2963ac7aa
neotmiravsego@gmail.com <neotmiravsego@gmail.com>
2019-09-20 14:26:11 +0200

    create and fix slider

---
+++ components/pages/blog/sliderBlock.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  .slider__blog
+    .slider__blog__img__wrap
+      img(src="../../../assets/images/blogslider.png",).slider__img
+    .slider__blog__content
+      .slider__blog__title__wrap
+        h2.slider__blog__title White shark monkeyface prickleback bluefish kuhli loach; large-e
+      .slider__blog__txt__wrap
+        p.slider__blog__txt__data 13 ноября 2018 г.<br>
+         <span class="slider__data__time">17:56</span>
+        p.slider__blog__txt 
+          | Сайт рыбатекст поможет дизайнеру, верстальщику,
+          | вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке,
+          | а начинающему оратору отточить навык публичных выступлений в домашних условиях.
+        p.slider__blog__author <span class="slider__author__txt">Автор:</span> Катя Анаприенко
+</template>
+<style lang="scss" scoped>
+.slider__blog {
+  display: flex;
+  color: black;
+  .slider__blog__img__wrap {
+    margin-right: 58px;
+    .slider__img {
+      display: block;
+      object-fit: cover;
+    }
+  }
+  .slider__blog__content {
+    .slider__blog__title__wrap {
+      margin-bottom: 10px;
+      max-width: 438px;
+      width: 100%;
+      line-height: $lineHeight4;
+    }
+    .slider__blog__author {
+      font-size: $fontSize1;
+      .slider__author__txt {
+        font-weight: bold;
+      }
+    }
+    .slider__blog__txt__wrap {
+      width: 100%;
+      font-size: $fontSize4;
+      text-align: justify;
+      line-height: 28px;
+      .slider__blog__txt {
+        margin-bottom: 17px;
+      }
+      .slider__blog__txt__data {
+        margin-bottom: 16px;
+        display: block;
+        font-size: $fontSizesm;
+        color: $neutral;
+        line-height: 17px;
+      }
+    }
+  }
+}
+</style>
--- components/slider.vue
+++ components/slider.vue
@@ -1,14 +1,168 @@
 <template lang="pug">
-  .sliderC
+  .slider
+    .slider__wrap(ref="sliderWrap")
+      ul.slider__line(ref="sliderLine")
+        li.slider__line__item
+          SliderBlock.slide
+        li.slider__line__item
+          SliderBlock.slide
+        li.slider__line__item
+          SliderBlock.slide
+        li.slider__line__item
+          SliderBlock.slide
+        li.slider__line__item
+          SliderBlock.slide
+        li.slider__line__item
+          SliderBlock.slide
+        li.slider__line__item
+          SliderBlock.slide
+    .slider__controls__panel
+      ul.slider__controls__list
+        li.slider__controls__list__item
+          span.slider__controls__dot
+        li.slider__controls__list__item
+          span.slider__controls__dot
+        li.slider__controls__list__item
+          span.slider__controls__dot
+        li.slider__controls__list__item
+          span.slider__controls__dot
+        li.slider__controls__list__item
+          span.slider__controls__dot
+        li.slider__controls__list__item
+          span.slider__controls__dot
+        li.slider__controls__list__item
+          span.slider__controls__dot
+      .slider__button__position__wrap
+        button.slider__controls__button__left(ref="prev")
+          img(src="../assets/images/Rectangle-left.png", alt="alt")
+        button.slider__controls__button__right#next(ref="next")
+          img(src="../assets/images/Rectangle-right.png", alt="alt").controls__button__right
+        button.slider__controls__button__read Читать
 </template>
 
-<style lang="scss">
-.sliderC {
+<script lang="JavaScript">
+import SliderBlock from '~/components/pages/blog/sliderBlock.vue'
+
+export default {
+  components: {
+   SliderBlock
+  },
+  data () {
+    return {
+      offset: 0
+    }
+  },
+  mounted() {
+    this.createSlider()
+  },
+  methods: {
+    createSlider () {
+      const { prev, next, sliderLine, sliderWrap } = this.$refs
+      const lineWidth = sliderWrap.offsetWidth + 40;
+      let { offset } = this
+
+      function prevHandler() {
+        if (offset < 0) {
+          offset = offset + lineWidth;
+          sliderLine.style.left = offset +'px';
+        }
+      }
+
+      function nextHandler() {
+        if (offset < -6660) {
+            return;
+        }
+        offset = offset - lineWidth;
+        sliderLine.style.left = offset + 'px';
+      }
+
+      prev.onclick = prevHandler
+      next.onclick = nextHandler
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.slider {
+  position: relative;
   margin: 0 auto;
   margin-bottom: 40px;
   width: 100%;
-  height: 352px;
   max-width: $widthContainer1;
-  background: black;
+  &__wrap {
+    overflow: hidden;
+  }
+  &__line {
+    display: flex;
+    position: relative;
+    transition: all ease 1s;
+    left: 0;
+    &__item {
+      margin-right: 40px;
+      width: 100%;
+      flex-shrink: 0;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+    &::after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+  }
+  // overflow: hidden;
+  .slider__controls__panel {
+    width: 100%;
+    max-width: 699px;
+    display: flex;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    justify-content: space-between;
+    align-items: center;
+    .slider__controls__list {
+      display: flex;
+      .slider__controls__dot {
+        display: block;
+        margin-left: 10px;
+        width: 7px;
+        height: 7px;
+        border: 1px solid $neutral;
+        border-radius: 50%;
+      }
+    }
+  }
+  .slider__button__position__wrap {
+    .slider__controls__button__left {
+      margin-right: 18px;
+      width: 30px;
+      height: 40px;
+      border-radius: 20px;
+      background: $white;
+      &:hover {
+        background: linear-gradient(90deg, $light 50%, transparent 50%);
+      }
+    }
+    .slider__controls__button__right {
+      margin-right: 40px;
+      width: 30px;
+      height: 40px;
+      border-radius: 20px;
+      background: $white;
+      &:hover {
+        background: linear-gradient(270deg, $light 50%, transparent 50%);
+      }
+    }
+    .slider__controls__button__read {
+      padding: 11px 50px;
+      font-size: $fontSize2;
+      color: $white;
+      background: #e0c0c3;
+      box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
+      border-radius: 30px;
+    }
+  }
 }
 </style>
