9024786c8d53ae56505d004fe63378a1978b8626
neotmiravsego@gmail.com <neotmiravsego@gmail.com>
2019-09-27 08:47:00 +0200

    fix code and delete styles

--- components/slider.vue
+++ components/slider.vue
@@ -4,28 +4,6 @@
       ul.slider__line(ref="sliderLine")
         li.slider__line__item(v-for="item of news")
           SliderBlock.slide(:slideData="item")
-    .slider__controls__panel
-      ul.slider__controls__list
-        li.slider__controls__list__item
-          span.slider__controls__dot
-        li.slider__controls__list__item
-          span.slider__controls__dot
-        li.slider__controls__list__item
-          span.slider__controls__dot
-        li.slider__controls__list__item
-          span.slider__controls__dot
-        li.slider__controls__list__item
-          span.slider__controls__dot
-        li.slider__controls__list__item
-          span.slider__controls__dot
-        li.slider__controls__list__item
-          span.slider__controls__dot
-      .slider__button__position__wrap
-        button.slider__controls__button__left(ref="prev")
-          img(src="../assets/images/Rectangle-left.png", alt="alt")
-        button.slider__controls__button__right#next(ref="next")
-          img(src="../assets/images/Rectangle-right.png", alt="alt").controls__button__right
-        button.slider__controls__button__read Читать
 </template>
 
 <script lang="JavaScript">
@@ -124,58 +102,6 @@ export default {
       content: '';
       display: block;
       clear: both;
-    }
-  }
-  // overflow: hidden;
-  .slider__controls__panel {
-    width: 100%;
-    max-width: 699px;
-    display: flex;
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    justify-content: space-between;
-    align-items: center;
-    .slider__controls__list {
-      display: flex;
-      .slider__controls__dot {
-        display: block;
-        margin-left: 10px;
-        width: 7px;
-        height: 7px;
-        border: 1px solid $neutral;
-        border-radius: 50%;
-      }
-    }
-  }
-  .slider__button__position__wrap {
-    .slider__controls__button__left {
-      margin-right: 18px;
-      width: 30px;
-      height: 40px;
-      border-radius: 20px;
-      background: $white;
-      &:hover {
-        background: linear-gradient(90deg, $light 50%, transparent 50%);
-      }
-    }
-    .slider__controls__button__right {
-      margin-right: 40px;
-      width: 30px;
-      height: 40px;
-      border-radius: 20px;
-      background: $white;
-      &:hover {
-        background: linear-gradient(270deg, $light 50%, transparent 50%);
-      }
-    }
-    .slider__controls__button__read {
-      padding: 11px 50px;
-      font-size: $fontSize2;
-      color: $white;
-      background: #e0c0c3;
-      box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
-      border-radius: 30px;
     }
   }
 }
